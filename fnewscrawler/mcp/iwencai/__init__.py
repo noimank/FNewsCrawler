6359c44ea598352947bb129210cf93d1f69e213c
noimank <noimank@163.com>
2025-08-03 14:26:19 +0800

    feat: 增加概念资金流功能

--- fnewscrawler/mcp/iwencai/__init__.py
+++ fnewscrawler/mcp/iwencai/__init__.py
@@ -2,5 +2,4 @@
 #将mcp工具暴露，利用python的导包机制完成工具注册进mcp_server
 import fnewscrawler.mcp.iwencai.crawl
 import fnewscrawler.mcp.iwencai.industry_funds
-
-
+import fnewscrawler.mcp.iwencai.concept_funds
--- fnewscrawler/spiders/iwencai/__init__.py
+++ fnewscrawler/spiders/iwencai/__init__.py
@@ -1,8 +1,8 @@
 from .crawl import iwencai_crawl_from_query
 from .login import  IwencaiLogin
 from .industry_funds import iwencai_industry_funds
+from .concept_funds import iwencai_concept_funds
 
 
-
-__all__ = ["iwencai_crawl_from_query", "IwencaiLogin", "iwencai_industry_funds"]
+__all__ = ["iwencai_crawl_from_query", "IwencaiLogin", "iwencai_industry_funds", "iwencai_concept_funds"]
 

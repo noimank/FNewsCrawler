8b5362d5cb07df34ce1e7c75aadda19c3c6153d1
noimank <noimank@163.com>
2025-08-03 10:39:35 +0800

    fix: 删除测试工具代码，修改工具title

--- fnewscrawler/mcp/iwencai/crawl.py
+++ fnewscrawler/mcp/iwencai/crawl.py
@@ -3,7 +3,7 @@ from fnewscrawler.spiders.iwencai import iwencai_crawl_from_query
 
 
 
-@mcp_server.tool()
+@mcp_server.tool(title="同花顺问财-专业新闻查询工具")
 async def iwencai_news_query(query: str, page_no: int = 1):
     """
     从同花顺问财(iWenCai)获取最新财经新闻的专业查询工具
@@ -78,37 +78,4 @@ async def iwencai_news_query(query: str, page_no: int = 1):
 
     }
     return news_info
-
-@mcp_server.tool()
-def iwencai_test(query):
-    """
-    iwencai_news_query 工具函数
-    ### Adding Components
-
-    Your converted MCP server is a full FastMCP instance, meaning you can add new tools, resources, and other components to it just like you would with any other FastMCP instance.
-
-    ```python {8-11}
-    # Assumes the FastAPI app from above is already defined
-    from fastmcp import FastMCP
-
-    # Convert to MCP server
-    mcp = FastMCP.from_fastapi(app=app)
-
-    # Add a new tool
-    @mcp.tool
-    def get_product(product_id: int) -> ProductResponse:
-
-        return products_db[product_id]
-
-    # Run the MCP server
-    if __name__ == "__main__":
-        mcp.run()
-    ```
-    用于查询iwencai新闻
-
-    :param query: 查询字符串
-    :return: 查询结果
-    """
-    return f"search {query}"
-
 

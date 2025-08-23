83fcc60080fcecffec81c7555ed1fee1a18e44fe
noimank <noimank@163.com>
2025-08-23 17:29:54 +0800

    feat: 添加东方财富获取指定股票最近一年的龙虎榜明细工具

--- test/spiders/eastmoney/dragon_tiger_details.py
+++ test/spiders/eastmoney/dragon_tiger_details.py
@@ -1,4 +1,4 @@
-from fnewscrawler.spiders.eastmoney import eastmoney_dragon_tiger_detail
+from fnewscrawler.spiders.eastmoney import eastmoney_dragon_tiger_detail, eastmoney_stock_dragon_tiger_detail
 import asyncio
 
 async def test_get_dragon_tiger_detail():
@@ -9,5 +9,17 @@ async def test_get_dragon_tiger_detail():
     table = await eastmoney_dragon_tiger_detail(rank_type, page_num)
     print(table)
 
+async def test_get_stock_dragon_tiger_detail():
+
+    # stock_code = "002214"
+    stock_code = "002214"
+
+    table = await eastmoney_stock_dragon_tiger_detail(stock_code)
+    print(table)
+
+
+
+
 if __name__ == '__main__':
-    asyncio.run(test_get_dragon_tiger_detail())
+    # asyncio.run(test_get_dragon_tiger_detail())
+    asyncio.run(test_get_stock_dragon_tiger_detail())

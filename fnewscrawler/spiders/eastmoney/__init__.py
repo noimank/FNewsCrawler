3145849983a4e5d700064e47b3b5eaee38f81d9b
noimank <noimank@163.com>
2025-08-23 09:21:23 +0800

    feat: 添加东方财富股票大宗交易每日明细查询工具

--- fnewscrawler/spiders/eastmoney/__init__.py
+++ fnewscrawler/spiders/eastmoney/__init__.py
@@ -2,5 +2,5 @@ from .login import EastMoneyLogin
 from .industry_funds import get_industry_history_funds_flow
 from .industry_funds import get_industry_stock_funds_flow
 from .big_market_funds import eastmoney_market_history_funds_flow
-
-__all__ = ["EastMoneyLogin", "get_industry_history_funds_flow", "get_industry_stock_funds_flow", "eastmoney_market_history_funds_flow"]
+from .block_trade import eastmoney_block_trade_detail
+__all__ = ["EastMoneyLogin", "get_industry_history_funds_flow", "get_industry_stock_funds_flow", "eastmoney_market_history_funds_flow", "eastmoney_block_trade_detail"]

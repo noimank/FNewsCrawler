72654711c25dd16570796451f391ccfb186edf7a
noimank <noimank@163.com>
2025-08-31 12:03:11 +0800

    feat: 添加akshare获取股票信息披露公告数据

--- fnewscrawler/spiders/akshare/__init__.py
+++ fnewscrawler/spiders/akshare/__init__.py
@@ -3,12 +3,13 @@ from .daily import ak_daily
 from .news import ak_news_cctv
 from .stock_comment import ak_stock_comment_detail
 from .news import ak_stock_news_em,ak_stock_news_main_cx
-
+from .report import ak_stock_zh_a_disclosure_report_cninfo
 __all__ = [
     "ak_daily",
     "ak_news_cctv",
     "ak_daily",
     "ak_stock_comment_detail",
     "ak_stock_news_em",
-    "ak_stock_news_main_cx"
+    "ak_stock_news_main_cx",
+    "ak_stock_zh_a_disclosure_report_cninfo"
 ]

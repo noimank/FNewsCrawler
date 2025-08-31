5a4e3a790d989fa163f021bb4cf6645631cda544
noimank <noimank@163.com>
2025-08-31 11:09:01 +0800

    feat: 添加akshare获取股票新闻数据

--- fnewscrawler/mcp/akshare/news.py
+++ fnewscrawler/mcp/akshare/news.py
@@ -1,4 +1,4 @@
-from fnewscrawler.spiders.akshare import ak_news_cctv
+from fnewscrawler.spiders.akshare import ak_news_cctv, ak_stock_news_em
 from fnewscrawler.mcp import mcp_server
 
 
@@ -15,3 +15,22 @@ def get_ak_news_cctv(date: str)->str:
     """
     markdown_table = ak_news_cctv(date)
     return markdown_table
+
+
+@mcp_server.tool(title="从akshare获取股票新闻数据")
+def get_ak_stock_news_em(stock_code: str)->str:
+    """从akshare获取股票新闻数据
+
+    来源：东方财富，最近100条相关新闻，新闻内容并非完整，只是截取的部分内容
+
+    Args:
+        stock_code: 股票代码，如'600000'
+
+    Returns:
+        包含新闻数据的markdown表格，列名包括：新闻标题、新闻内容、发布时间、文章来源
+    """
+    markdown_table = ak_stock_news_em(stock_code)
+    return markdown_table
+
+
+
--- test/spiders/akshare/news.py
+++ test/spiders/akshare/news.py
@@ -1,9 +1,15 @@
-from fnewscrawler.spiders.akshare import ak_news_cctv
+from fnewscrawler.spiders.akshare import ak_news_cctv,ak_stock_news_em
 
 
 def test_ak_news_cctv():
     news_cctv_df = ak_news_cctv('20250829')
     print(news_cctv_df)
 
+def test_ak_stock_news_em():
+    stock_news_em_df = ak_stock_news_em('000001')
+    print(stock_news_em_df)
+
 if __name__ == '__main__':
-    test_ak_news_cctv()
+    # test_ak_news_cctv()
+
+    test_ak_stock_news_em()

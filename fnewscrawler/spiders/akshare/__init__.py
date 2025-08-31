e9ad12307965c2be98bf4584565cd74ecebbc2de
noimank <noimank@163.com>
2025-08-31 10:02:46 +0800

    feat: 添加akshare股票机构参与度数据

--- fnewscrawler/spiders/akshare/__init__.py
+++ fnewscrawler/spiders/akshare/__init__.py
@@ -1,11 +1,12 @@
 from .chip import stock_cyq_em
 from .daily import ak_daily
 from .news import ak_news_cctv
-
+from .stock_comment import ak_stock_comment_detail
 
 
 __all__ = [
     "ak_daily",
     "ak_news_cctv",
-    "ak_daily"
+    "ak_daily",
+    "ak_stock_comment_detail"
 ]

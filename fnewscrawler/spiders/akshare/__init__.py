0d2b42e053e3a04d9ed026450f3965a94abfe765
noimank <noimank@163.com>
2025-08-31 11:33:27 +0800

    feat: 添加akshare获取财经内容精选数据

--- fnewscrawler/spiders/akshare/__init__.py
+++ fnewscrawler/spiders/akshare/__init__.py
@@ -2,12 +2,13 @@ from .chip import stock_cyq_em
 from .daily import ak_daily
 from .news import ak_news_cctv
 from .stock_comment import ak_stock_comment_detail
-from .news import ak_stock_news_em
+from .news import ak_stock_news_em,ak_stock_news_main_cx
 
 __all__ = [
     "ak_daily",
     "ak_news_cctv",
     "ak_daily",
     "ak_stock_comment_detail",
-    "ak_stock_news_em"
+    "ak_stock_news_em",
+    "ak_stock_news_main_cx"
 ]
--- fnewscrawler/spiders/akshare/news.py
+++ fnewscrawler/spiders/akshare/news.py
@@ -1,4 +1,5 @@
 import akshare as ak
+import pandas as pd
 
 
 def ak_news_cctv(date: str)->str:
@@ -34,4 +35,31 @@ def ak_stock_news_em(stock_code: str)->str:
     #丢弃一些列
     stock_news_em_df = stock_news_em_df.drop(columns=["关键词", "新闻链接"])
     markdown_table = stock_news_em_df.to_markdown(index=False)
+    return markdown_table
+
+
+
+def ak_stock_news_main_cx(start_date: str = "20250829")->str:
+    """获取akshare财经内容精选数据
+
+    来源： 财新网-财新数据通-内容精选,返回start_date之后的新闻
+
+    Args:
+        start_date: 开始日期，格式'YYYYMMDD'，如'20250829'
+
+    Returns:
+        包含新闻数据的markdown表格，列名包括：新闻标题、新闻内容、发布时间、文章来源
+    """
+    stock_news_main_cx_df = ak.stock_news_main_cx()
+    # 转换日期格式用于比较
+    start_date = pd.to_datetime(start_date)
+    # 转换pub_time为日期时间格式
+    stock_news_main_cx_df["pub_time"] = pd.to_datetime(stock_news_main_cx_df["pub_time"])
+    # 丢弃一些列
+    stock_news_main_cx_df = stock_news_main_cx_df[stock_news_main_cx_df["pub_time"] >= start_date]
+    stock_news_main_cx_df = stock_news_main_cx_df.drop(columns=["url", "interval_time"])
+    # 重命名列
+    stock_news_main_cx_df = stock_news_main_cx_df.rename(columns={"tag": "新闻标签", "summary": "新闻内容", "pub_time": "发布时间"})
+    # 转换为markdown表格
+    markdown_table = stock_news_main_cx_df.to_markdown(index=False)
     return markdown_table

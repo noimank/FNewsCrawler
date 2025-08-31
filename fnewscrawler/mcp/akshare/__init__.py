d90e09e48c662a3f1cc66e2fe4d80cf8cbe4e1e7
noimank <noimank@163.com>
2025-08-31 12:03:11 +0800

    feat: 添加akshare获取股票信息披露公告数据

--- fnewscrawler/mcp/akshare/__init__.py
+++ fnewscrawler/mcp/akshare/__init__.py
@@ -3,4 +3,5 @@ import fnewscrawler.mcp.akshare.chip
 import fnewscrawler.mcp.akshare.daily
 import fnewscrawler.mcp.akshare.news
 import fnewscrawler.mcp.akshare.stock_comment
+import fnewscrawler.mcp.akshare.report
 

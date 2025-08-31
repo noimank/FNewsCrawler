3c4db3398d2c831f56426ae7e8b7b0385772b60e
noimank <noimank@163.com>
2025-08-31 11:09:01 +0800

    feat: 添加akshare获取股票新闻数据

--- fnewscrawler/spiders/akshare/news.py
+++ fnewscrawler/spiders/akshare/news.py
@@ -17,3 +17,21 @@ def ak_news_cctv(date: str)->str:
     news_cctv_df = news_cctv_df.rename(columns={"date": "日期", "content": "内容"})
     markdown_table = news_cctv_df.to_markdown(index=False)
     return markdown_table
+
+
+def ak_stock_news_em(stock_code: str)->str:
+    """获取akshare股票新闻数据
+
+    来源：东方财富，最近100条相关新闻，新闻内容并非完整，只是截取的部分内容
+
+    Args:
+        stock_code: 股票代码，如'600000'
+
+    Returns:
+        包含新闻数据的DataFrame，列名包括：新闻标题、新闻内容、发布时间、文章来源
+    """
+    stock_news_em_df = ak.stock_news_em(symbol=stock_code)
+    #丢弃一些列
+    stock_news_em_df = stock_news_em_df.drop(columns=["关键词", "新闻链接"])
+    markdown_table = stock_news_em_df.to_markdown(index=False)
+    return markdown_table
